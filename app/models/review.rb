98f15100276e0fdfedfc27b99d861bf4334eec4a
Louis Deboutin <louis.deboutin@gmail.com>
2019-11-20 16:36:17 +0100

    1

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates_numericality_of :rating
+  validates_inclusion_of :rating, :in => 0..5
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Restaurant.create(name: "Alexandre", address: "1 avenue Alex", category: "french")
+  Restaurant.create(name: "Corentin", address: "2 rue Corentin", category: "italian")
+  Restaurant.create(name: "Ben", address: "3 boulevard Ben", category: "japanese")
+  Restaurant.create(name: "Justine", address: "4 avenue Justine", category: "chinese")
+  Restaurant.create(name: "Marc", address: "5 avenue Marc", category: "belgian")
